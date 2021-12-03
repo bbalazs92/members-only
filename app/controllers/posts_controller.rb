bbb433639ae7bd3158a95c264911993ead00f939
Balazs <balazs.birta@gmail.com>
2021-12-03 12:22:02 +0100

    Added model relationships

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = current_member.posts.build
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- 
+  get "/new", to: "posts#new"
   
   get "/users", to: "users#index"
   get "/users/:id", to: "users#show"
